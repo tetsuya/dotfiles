d62f7cfc62dcb11afa861a666de0e8ec786fc406
tetsuya <tetsuya@users.noreply.github.com>
2016-01-13 11:39:51 +0900

    Add OSX settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install ansible
 cd ./ansible_files
-ansible-playbook -i hosts localhost.yml
+ansible-playbook -i hosts localhost.yml --ask-sudo-pass
