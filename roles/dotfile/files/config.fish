ab8167f57a4b1c8cd90eee2829688899aff235d6
tetsuya <hello+git@tetsuya.xyz>
2019-06-06 17:10:32 +0900

    Migrate fisher to v3

--- roles/dotfile/files/config.fish
+++ roles/dotfile/files/config.fish
@@ -1,9 +1,9 @@
-balias be 'bundle exec'
-balias bes 'bundle exec spring'
-balias dc 'docker-compose'
-balias git 'hub'
-balias ls 'ls -GAF'
-balias rm 'rmtrash'
+alias be="bundle exec"
+alias git="hub"
+alias ls="ls -GAF"
+alias pwf="pfd"
+alias rm="rmtrash"
+alias wifi="wifi-password"
 
 set -gx LANG en_US.UTF-8
 set -gx LC_ALL en_US.utf-8
