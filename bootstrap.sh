cd9054899611553351e70f324697356d66aa29ee
tetsuya <tetsuya@users.noreply.github.com>
2019-10-18 01:40:42 +0900

    Change option

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install ansible
-ansible-playbook -i hosts playbook.yml --ask-sudo-pass
+ansible-playbook -i hosts playbook.yml --ask-become-pass
