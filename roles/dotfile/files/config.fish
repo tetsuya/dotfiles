8a1cbb354c1089dc55bca54d822fcd18cf6ca6b3
tetsuya <tetsuya@users.noreply.github.com>
2020-04-03 15:51:02 +0900

    Update alias

--- roles/dotfile/files/config.fish
+++ roles/dotfile/files/config.fish
@@ -1,12 +1,11 @@
 alias be="bundle exec"
+alias bi="bundle install"
 alias do="docker"
-alias dorm="dk rm (dk ps -a -f status=exited -q)"
-alias git="hub"
 alias ku="kubectl"
 alias ls="ls -GAF"
 alias pwf="pfd"
 alias rm="rmtrash"
 alias wifi="wifi-password"
 
-set -gx LANG en_US.UTF-8
-set -gx LC_ALL en_US.utf-8
+set --global --export LANG en_US.UTF-8
+set --global --export LC_ALL en_US.utf-8
