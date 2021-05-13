4abda1b67528afda9f7cdf5e6269ca26e489a6a8
Tetsuya Kaneko <tetsuya@users.noreply.github.com>
2021-05-13 16:30:13 +0900

    Update abbr

--- roles/dotfile/files/config.fish
+++ roles/dotfile/files/config.fish
@@ -6,14 +6,24 @@ abbr -a g git
 abbr -a ga git add
 abbr -a gb git branch
 abbr -a gc git commit
+abbr -a gcm git commit -m
 abbr -a gca git commit --amend
 abbr -a gco git checkout
 abbr -a gd git diff
 abbr -a gl git log
 abbr -a gls git log --name-status --pretty=fuller
 abbr -a gp git push
+abbr -a gr git reset
+abbr -a grh git reset --hard
+abbr -a grs git reset --soft
+abbr -a gri git rebase -i
+abbr -a gra git rebase --abort
+abbr -a grc git rebase --continue
 abbr -a gs git status
-abbr -a gsh git stash
+abbr -a gst git stash
+abbr -a gsl git stash list
+abbr -a gsp git stash pop
+abbr -a gss git stash show
 
 alias be="bundle exec"
 alias bi="bundle install"
