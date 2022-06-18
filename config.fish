07e246136ea1f0b0f4e39a66d88bd83bfbb5f74c
Tetsuya Kaneko <tetsuya@users.noreply.github.com>
2022-06-18 18:59:37 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -4,28 +4,31 @@ abbr -a c code
 abbr -a d docker
 abbr -a h heroku
 
+abbr -a f flutter
 abbr -a g git
 abbr -a ga git add
 abbr -a gb git branch
 abbr -a gc git commit
-abbr -a gcm git commit -m
 abbr -a gca git commit --amend
+abbr -a gcm git commit -m
 abbr -a gco git checkout
+abbr -a gcp git cherry-pick
 abbr -a gd git diff
 abbr -a gl git log
 abbr -a gls git log --name-status --pretty=fuller
 abbr -a gp git push
 abbr -a gr git reset
-abbr -a grh git reset --hard
-abbr -a grs git reset --soft
-abbr -a gri git rebase -i
 abbr -a gra git rebase --abort
 abbr -a grc git rebase --continue
+abbr -a grh git reset --hard
+abbr -a gri git rebase -i
+abbr -a grs git reset --soft
 abbr -a gs git status
-abbr -a gst git stash
+abbr -a gsh git stash
 abbr -a gsl git stash list
 abbr -a gsp git stash pop
 abbr -a gss git stash show
+abbr -a pub flutter pub
 
 alias be="bundle exec"
 alias bi="bundle install"
